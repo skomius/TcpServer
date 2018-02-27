e23490453a6fe628e255be140725907cf2b615b3
skomius <skomantas.juskys@outlook.com>
2018-02-27 08:08:19 +0200

    client

--- TcpServer/Program.cs
+++ TcpServer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TcpServer
@@ -10,6 +11,19 @@ namespace TcpServer
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Iveskite direktorija (nenaudokite backslash \\ ): ");
+            string path = Console.ReadLine();
+
+            Console.WriteLine("Iveskite porto numeri: ");
+            int port;
+            int.TryParse(Console.ReadLine(), out port);
+
+
+            new Thread(() => Server.StartServer(port)).Start();
+            Thread.Sleep(500);
+            new Thread(() => Client.StartClient(port, path)).Start();
         }
     }
 }
+
---
+++ TcpServer/Server.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TcpServer
+{
+    class Server
+    {
+        public static ManualResetEvent clientConnected =
+        new ManualResetEvent(false);
+
+        public static void StartServer(int port)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start(100);
+
+            while (true)
+            {
+                clientConnected.Reset();
+                //Console.WriteLine("Waiting for a connection...");
+
+                listener.BeginAcceptSocket(
+                    new AsyncCallback(Callback), listener);
+                clientConnected.WaitOne();
+            }
+
+        }
+
+        public static void Callback(IAsyncResult ar)
+        {
+            TcpListener listener = (TcpListener)ar.AsyncState;
+            Socket clientSocket = listener.EndAcceptSocket(ar);
+            //Console.WriteLine("Client connected completed");
+
+            clientConnected.Set();
+
+            byte[] buffer = new byte[1024];
+            int bytesRecieved = clientSocket.Receive(buffer);
+
+            string path = Encoding.ASCII.GetString(
+                buffer, 0, bytesRecieved);
+
+            
+            string[] files = Directory.GetFiles(path);
+            StringBuilder names = new StringBuilder();
+            foreach (string s in files)
+            {
+                names.Append(Regex.Match(s, @"\\(?:.(?!\\))+$").Value + '\n');
+            }
+
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(names.ToString());
+            clientSocket.Send(bytesToSend);
+
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+
+        }
+    }
+}
